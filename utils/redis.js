8252486632d990e5cc67a1ffa5b85858cb66be0f
Dubemxe <dubemxe@gmail.com>
2024-08-05 09:44:56 +0000

    Provided Files

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// Define a class 
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // Verify connection
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // Get value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // Set key value
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // Delete key value
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
